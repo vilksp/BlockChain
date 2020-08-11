644588f95a416396304c4aa63d9e27cf349d6265
vilksp <v.kasperavicius1@gmail.com>
2020-08-11 22:29:30 +0300

    final comit

--- src/StringUtil.java
+++ src/StringUtil.java
@@ -1,3 +1,5 @@
+import com.google.gson.GsonBuilder;
+
 import java.security.MessageDigest;
 
 public class StringUtil {
@@ -19,4 +21,14 @@ public class StringUtil {
             throw new RuntimeException(e);
         }
     }
+
+    public static String getJson(Object o) {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+    }
+
+    //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
+    }
+
 }
